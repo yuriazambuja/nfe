c3476ae6a19d56be431c54c1d44b79d28b38284c
yuriazambuja <46815214+yuriazambuja@users.noreply.github.com>
2019-07-08 13:52:42 -0300

    Add files via upload

--- www/custom/js/javascript.js
+++ www/custom/js/javascript.js
@@ -7,10 +7,10 @@ var app  = new Framework7({
   id: 'br.com.azamba.nfe', // App bundle ID
   name: 'Recebimento', // App name
   theme: 'ios', // Automatic theme detection
-  view: {
-    pushState: true,
-    pushStateSeparator:'#',
-  },
+  //view: {
+  //  pushState: true,
+  //  pushStateSeparator:'#',
+  //},
   // App root data
   data: function () {
     return {
@@ -60,35 +60,53 @@ var app  = new Framework7({
       }
     },
     verifica:function(chave){
-        if(chave.length!=44){
-            return(false);
-        }else{
-            fator = [2,3,4,5,6,7,8,9];
-            digito = 0;
-            for(i=0;i<43;i++){
-                digito+=chave[42-i]*fator[i%fator.length];
-            };
-            digito%=11;
-            if (digito<=1) {
-                digito=0;
-            } else {
-                digito=(11-digito);
-            }
-            return(chave[43]==digito);
+      chave = chave.replace(/[^\d]+/g,'');
+      agora = new Date();
+      if(chave.length!=44){
+        return(false); // TAMANHO INVALIDO
+      }else if([11,12,13,14,15,16,17,21,22,23,24,25,26,27,28,29,31,32,33,35,41,42,43,50,51,52,53].indexOf(Number(chave.substring(0,2)))<0){
+        return(false); // UF IBGE INVALIDA
+      }else if(Number(chave.substring(4,6))>12){
+        return(false); // MES INVALIDO
+      }else if(Number(chave.substring(2,6))>((agora.getYear()-100)*100)+agora.getMonth()+1){
+        return(false); // ANO MES FUTURO
+      }else{
+        fator = [2,3,4,5,6,7,8,9];
+        digito = 0;
+        for(i=0;i<43;i++){
+          digito+=chave[42-i]*fator[i%fator.length];
+        };
+        digito%=11;
+        if (digito<=1) {
+          digito=0;
+        } else {
+          digito=(11-digito);
         }
+        return(chave[43]==digito);
+      }
     },
     consulta:function(consulta){
-        _ = this;
-        _.preloader.show();
-        try {
-            _.data.api.MPL_OR_ValidaNFe(function(status,result){
-                _.preloader.hide();
-                _.dialog.alert(result.Status);
-            },{ChaveNFe:consulta});
-        }catch(error){
-            _.preloader.hide();
-            _.dialog.alert(error);
-        }
+      _ = this;
+      _.preloader.show();
+      try {
+        _.data.api.MPL_OR_ValidaNFe(function(status,result){ // 35190533194978000352550100001559521180715907
+          _.preloader.hide();
+          if(status==200){
+            if(result.Status=='A'){
+              _.dialog.confirm("CONFIRMA RECEBIMENTO DA NF-E?",null,function(){
+                _.dialog.alert("NF-E RECEBIDA COM SUCESSO");
+              });
+            }else{
+              _.dialog.alert("NF-E NÃO LIBERADA PARA RECEBIMENTO: "+result.Status);
+            }
+          }else{
+            alert("ERRO DE COMUNICAÇÃO: "+status);
+          }
+        },{ChaveNFe:consulta});
+      }catch(error){
+        _.preloader.hide();
+        _.dialog.alert(error);
+      }
     },
     manual:function(){
       _ = this;
@@ -100,30 +118,31 @@ var app  = new Framework7({
       }
     },
     automatico:function(){
+      _ = this;
       cordova.plugins.barcodeScanner.scan(
         function (result) {
           if(!result.cancelled){
             if(result.text=="123"||_.methods.verifica(result.text)){
               _.methods.consulta(result.text);
             }else{
-              app.dialog.alert("O CÓDIGO DE BARRAS LIDO NÃO É CHAVE DE ACESSO",null,_.methods.automatico);
+              _.dialog.alert("O CÓDIGO DE BARRAS LIDO NÃO É CHAVE DE ACESSO",null,_.methods.automatico);
             }
           }
         },
         function (error) {
-          app.dialog.alert("ERRO DESCONHECIDO:" + error);
+          _.dialog.alert("ERRO DESCONHECIDO:" + error);
         },
         {
           //preferFrontCamera : true, // iOS and Android
           //showFlipCameraButton : true, // iOS and Android
           //saveHistory: true, // Android, save scan history (default false)
-          formats : "CODE_128,ITF", //"QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          showTorchButton : false, // iOS and Android
+          formats: "CODE_128,ITF", //"QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          showTorchButton: false, // iOS and Android
           torchOn: false, // Android, launch with the torch switched on (if available)
-          prompt : " ",
+          prompt: " ",
           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          disableAnimations : true, // iOS
+          disableAnimations: true, // iOS
           disableSuccessBeep: false // iOS and Android
         }
       );
